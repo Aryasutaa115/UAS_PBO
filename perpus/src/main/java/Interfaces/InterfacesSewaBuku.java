f1cd21c62a0f69a6fb02eeca3c8f3e29b7b746e4
Aryasutaa115 <sutaa239@gmail.com>
2021-07-12 17:46:10 +0700

    Perpus XYZ

---
+++ perpus/src/main/java/Interfaces/InterfacesSewaBuku.java
@@ -0,0 +1,10 @@
+
+package Interfaces;
+
+import Model.mTable; /*Untuk memanggil mTable.java dalam package Model*/
+import java.util.List; /*Untuk memanggil package List pada java */
+
+
+public interface InterfacesSewaBuku {
+    public List<mTable> GetAll();
+}
---
+++ perpus/src/main/java/Model/mTable.java
@@ -0,0 +1,13 @@
+
+package Model;
+
+
+public class mTable {
+    public int id;
+    public String judul;
+    public String tanggal_pinjam;
+    public String tanggal_kembali;
+    public String tanggal_harus_kembali;
+    public int denda;
+    public int biaya_sewa;
+}
